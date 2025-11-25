324f45c059c68a53a5b6e89d2c504b2a5db03867
Forkxel <pavel.halik06@gmail.com>
2025-11-25 23:13:05 +0100

    Created basic server.

---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer;
+
+public class ChatServer
+{
+    private TcpListener Listener;
+    private Thread thread;
+    private bool running = true;
+
+    public ChatServer(int port)
+    {
+        Listener = new TcpListener(IPAddress.Any, port);
+        thread = new Thread(AcceptClient);
+    }
+
+    public void AcceptClient()
+    {
+        while (running)
+        {
+            try
+            {
+                var client = Listener.AcceptTcpClient();
+                Console.WriteLine("Client connected " + client.Client.RemoteEndPoint);
+                
+                client.Close();
+            }
+            catch (SocketException e) when (!running) { }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    public void Start()
+    {
+        Listener.Start();
+        thread.Start();
+        Console.WriteLine("Server started");
+    }
+
+    public void Stop()
+    {
+        running = false;
+        Listener.Stop();
+        thread.Join(500);
+    }
+}
