147bedb6debfa0d6fa49c97e1de58b80e8b35ea0
Forkxel <pavel.halik06@gmail.com>
2025-11-25 23:21:30 +0100

    Added ClientHandler.cs

---
+++ ChatServer/ClientHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer;
+
+public class ClientHandler
+{
+    private TcpClient client;
+    private Thread thread;
+
+    public ClientHandler(TcpClient client)
+    {
+        client = client;
+        thread = new Thread(Run);
+        thread.IsBackground = true;
+        thread.Start();
+    }
+
+    private void Run()
+    {
+        try
+        {
+            using var stream = client.GetStream();
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+            using var writer = new StreamWriter(stream, Encoding.UTF8);
+
+            writer.AutoFlush = true;
+
+            writer.WriteLine("Type and it will echo");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                writer.WriteLine(line);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
