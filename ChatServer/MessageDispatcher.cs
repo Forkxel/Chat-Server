fe8aac625fb761076baee0d00ff8471263be9fcd
Forkxel <pavel.halik06@gmail.com>
2025-11-26 17:38:02 +0100

    Created class MessageDispatcher.cs

---
+++ ChatServer/MessageDispatcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Concurrent;
+
+namespace ChatServer;
+
+public class MessageDispatcher
+{
+    private BlockingCollection<Message> queue = new();
+    private Thread thread;
+    private bool running = true;
+
+    public MessageDispatcher()
+    {
+        thread = new Thread(Send);
+        thread.Start();
+    }
+
+    public void Enqueue(Message message)
+    {
+        queue.Add(message);
+    }
+
+    private void Send()
+    {
+        while (running || queue.Count > 0)
+        {
+            try
+            {
+                var message = queue.Take();
+                Console.WriteLine(message.Sender + ": " + message.Text);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    public void Stop()
+    {
+        running = false;
+        queue.CompleteAdding();
+        thread.Join(500);
+    }
+}
