4ac5bc2cb2989699766e03f813debb0fe47680a8
Forkxel <pavel.halik06@gmail.com>
2025-11-26 20:12:52 +0100

    Created Room and RoomManager

---
+++ ChatServer/Rooms/Room.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+
+namespace ChatServer.Rooms;
+
+public class Room
+{
+    public string Name { get; set; }
+    private ConcurrentDictionary<ClientHandler, byte> members = new();
+
+    public Room(string name)
+    {
+        Name = name;
+    }
+
+    public void AddMember(ClientHandler handler)
+    {
+        members[handler] = 0;
+    }
+
+    public void RemoveMember(ClientHandler handler)
+    {
+        members.TryRemove(handler, out _);
+    }
+
+    public IEnumerable<ClientHandler> GetMembers()
+    {
+        return members.Keys;
+    }
+}
---
+++ ChatServer/Rooms/RoomManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+namespace ChatServer.Rooms;
+
+public class RoomManager
+{
+    private ConcurrentDictionary<string, Room> rooms = new();
+
+    public Room GetOrCreateRoom(string roomName)
+    {
+        return rooms.GetOrAdd(roomName, n => new Room(roomName));
+    }
+
+    public IEnumerable<string> GetAllRooms()
+    {
+        return rooms.Keys;
+    }
+}
