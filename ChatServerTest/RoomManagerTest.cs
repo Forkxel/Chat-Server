3de84dde9cfe5fe05a59c450a8468d18f79de076
Forkxel <pavel.halik06@gmail.com>
2025-12-04 22:14:21 +0100

    Created RoomManager tests.

---
+++ ChatServerTest/RoomManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using ChatServer.Rooms;
+
+namespace ChatServerTest;
+
+public class RoomManagerTest
+{
+    [Fact]
+    public void CreateRoom()
+    {
+        var manager = new RoomManager();
+        var room = manager.GetOrCreateRoom("general");
+        
+        Assert.Equal("general", room.Name);
+    }
+
+    [Fact]
+    public void GetRoom()
+    {
+        var manager = new RoomManager();
+        var room1 = manager.GetOrCreateRoom("general");
+        var room2 = manager.GetOrCreateRoom("general");
+        
+        Assert.Same(room2, room1);
+    }
+}
